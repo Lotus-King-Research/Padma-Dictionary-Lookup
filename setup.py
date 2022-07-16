45996221b12a2f1fbc09478442fb9ce11212d629
Mikko Kotila <upstairs@Mikkos-Mac-mini.local>
2022-07-16 12:07:58 +0300

    allow dictionary files to be zip

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ MAINTAINER_EMAIL = 'mailme@mikkokotila.com'
 URL = 'http://padma.io'
 LICENSE = 'MIT'
 DOWNLOAD_URL = 'https://github.com/Lotus-King-Research/Tibetan-Lookup'
-VERSION = '0.6.1'
+VERSION = '0.7'
 
 try:
     from setuptools import setup
